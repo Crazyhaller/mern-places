d7ff05a31c350a05a8c0c967ec4a2969b72d0d44
crazyhaller <suvigyamadrid@gmail.com>
2023-03-22 19:20:35 +0530

    Initial setup of the project

---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,17 @@
+import UsersList from '../components/UsersList'
+
+function Users() {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Suvigya Mishra',
+      image:
+        'https://images.pexels.com/photos/10080749/pexels-photo-10080749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      places: 4,
+    },
+  ]
+
+  return <UsersList items={USERS} />
+}
+
+export default Users
