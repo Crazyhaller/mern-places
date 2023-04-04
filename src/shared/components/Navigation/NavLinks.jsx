7e8d917af31c30f2000dfab0207e1bf2e17aaf3a
crazyhaller <suvigyamadrid@gmail.com>
2023-04-04 19:35:14 +0530

    listen to the context changes

--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { AuthContext } from '../../context/auth-context'
 import './NavLinks.css'
 
 function NavLinks() {
+  const auth = useContext(AuthContext)
+
   return (
     <ul className="nav-links">
       <li>
@@ -9,15 +13,21 @@ function NavLinks() {
           All Users
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">My Places</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">Add Place</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">Authenticate</NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">Add Place</NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">Authenticate</NavLink>
+        </li>
+      )}
     </ul>
   )
 }
